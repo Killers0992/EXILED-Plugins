76a0cec602f6cf1a806f7043d7b1eb7f3147abe9
Killers0992 <38152961+Killers0992@users.noreply.github.com>
2021-02-20 19:55:35 +0100

    Delete storage/framework/views directory

--- storage/framework/views/3fcdb36c1f67475e7f4c3c06d9a5134ef4e8803f.php
+++
@@ -1,3 +0,0 @@
-<footer class="main-footer">
-    <?php echo $__env->yieldContent('footer'); ?>
-</footer><?php /**PATH /var/www/plugins/resources/views/vendor/adminlte/partials/footer/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/7feb0882ec9f8b840d5a93388679f006056b1d4f.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item">
-    <a class="nav-link" href="<?php echo e(route('auth.steam')); ?>">
-        Login
-        <i class="fa fa-fw fa-user"></i>
-    </a>
-</li><?php /**PATH /var/www/plugins/resources/views/vendor/adminlte/partials/navbar/menu-item-login-link.blade.php ENDPATH**/ ?>
