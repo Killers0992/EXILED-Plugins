cc8322c790f7f5248407cdd3a159bb80905369d3
Killers0992 <killers0992@gmail.com>
2021-03-13 16:48:11 -0500

    Le fix

--- resources/views/viewpluginfiles.blade.php
+++ resources/views/viewpluginfiles.blade.php
@@ -20,7 +20,7 @@
                         <code><span class="text-xs text-gray-500">Created <abbr>{{$plugin->creation_date}}</abbr></span></code>
                         <code><span class="text-xs text-gray-500">Exiled Version: <abbr>{{$plugin->latest_exiled_version}}</abbr></span></code>
                           
-                        @if (Auth::user()->allowedPlugin('edit.plugin', $plugin) || Auth::user()->hasPermission('edit.plugin.admin'))
+                        @if (is_null(Auth::user()) ? false : (Auth::user()->allowedPlugin('edit.plugin', $plugin) || Auth::user()->hasPermission('edit.plugin.admin')))
                         <form method="get" action="{{route('plugin.edit', ['id' => $plugin->id])}}">
                             <button type="submit" class="btn btn-block btn-primary bg-purple btn-xs" style="width: 150px; float: right;">Edit</button>
                         </form>
@@ -99,7 +99,7 @@
                                     <td>{{$file->downloads_count}}</td>
                                     <td class="text-left">
 
-                                        @if (Auth::user()->allowedPlugin('delete.file', $plugin) || Auth::user()->hasPermission('delete.file.admin'))
+                                        @if (is_null(Auth::user()) ? false : (Auth::user()->allowedPlugin('delete.file', $plugin) || Auth::user()->hasPermission('delete.file.admin')))
                                         <form role="form" action="{{ route('plugin.delete.file', ['id' => $plugin->id]) }}" method="post">
                                             @csrf
                                             <input hidden="true" name="fileid" value="{{$file->file_id}}">
@@ -160,7 +160,7 @@
             </div>
         </div>
     </div>
-    @if (Auth::user()->allowedPlugin('upload.file', $plugin) || Auth::user()->hasPermission('upload.file.admin'))
+    @if (is_null(Auth::user()) ? false : (Auth::user()->allowedPlugin('upload.file', $plugin) || Auth::user()->hasPermission('upload.file.admin')))
     <div class="row">
         <div class="col-12">
             <div class="card">
